22a5e6efbcbab81196e348474b56f9b3963b5186
Alex Van Camp <vancamp.alex@gmail.com>
2017-05-25 14:22:45 -0500

    lint

--- dashboard/elements/time-validator/time-validator.js
+++ dashboard/elements/time-validator/time-validator.js
@@ -9,7 +9,7 @@
 		],
 
 		validate(value) {
-			// this regex validates incomplete times (by design)
+			// This regex validates incomplete times (by design)
 			return !value || value.match(/^[0-9]{0,2}:[0-9]{0,2}$/);
 		}
 	});
