167ce5bb2b53854c8f1b33405f6154535c9e956a
Alex Van Camp <vancamp.alex@gmail.com>
2017-06-03 14:34:57 -0500

    refactor(extension): change how load errors are handled to allow Sentry/Raven to do its thing

--- extension/index.js
+++ extension/index.js
@@ -12,102 +12,34 @@ module.exports = function (nodecg) {
 		nodecg.log.warn('WARNING! useMockData is true, you will not receive real data from the tracker!');
 	}
 
-	try {
-		require('./obs');
-	} catch (e) {
-		nodecg.log.error('Failed to load "obs" lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
-		require('./schedule');
-	} catch (e) {
-		nodecg.log.error('Failed to load "schedule" lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
-		require('./prizes');
-	} catch (e) {
-		nodecg.log.error('Failed to load "prizes" lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
-		require('./bids');
-	} catch (e) {
-		nodecg.log.error('Failed to load "bids" lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
-		require('./total');
-	} catch (e) {
-		nodecg.log.error('Failed to load "total" lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
-		require('./timekeeping');
-	} catch (e) {
-		nodecg.log.error('Failed to load "timekeeping" lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
-		require('./advertisements');
-	} catch (e) {
-		nodecg.log.error('Failed to load "advertisements" lib:', e.stack);
-		process.exit(1);
-	}
+	require('./obs');
+	require('./schedule');
+	require('./prizes');
+	require('./bids');
+	require('./total');
+	require('./timekeeping');
+	require('./advertisements');
+	require('./nowplaying');
+	require('./countdown');
 
 	if (nodecg.bundleConfig.twitter.userId) {
-		try {
-			require('./twitter');
-		} catch (e) {
-			nodecg.log.error('Failed to load "twitter" lib:', e.stack);
-			process.exit(1);
-		}
+		require('./twitter');
 	} else {
-		nodecg.log.error('"twitter" is not defined in cfg/sgdq17-layouts.json! ' +
+		nodecg.log.warn('"twitter" is not defined in cfg/sgdq17-layouts.json! ' +
 			'Twitter integration will be disabled.');
 	}
 
 	if (nodecg.bundleConfig.osc.address) {
-		try {
-			require('./osc');
-		} catch (e) {
-			nodecg.log.error('Failed to load "osc" lib:', e.stack);
-			process.exit(1);
-		}
+		require('./osc');
 	} else {
-		nodecg.log.error('"osc" is not defined in cfg/sgdq17-layouts.json! ' +
+		nodecg.log.warn('"osc" is not defined in cfg/sgdq17-layouts.json! ' +
 			'Behringer X32 OSC integration will be disabled.');
 	}
 
-	if (Object.keys(nodecg.bundleConfig.firebase).length === 0) {
-		nodecg.log.error('"firebase" is not defined in cfg/sgdq17-layouts.json! ' +
-			'The interview question system (Lightning Round) will be disabled.');
+	if (Object.keys(nodecg.bundleConfig.firebase).length > 0) {
+		require('./interview');
 	} else {
-		try {
-			require('./interview');
-		} catch (e) {
-			nodecg.log.error('Failed to load "interview" lib:', e.stack);
-			process.exit(1);
-		}
-	}
-
-	try {
-		require('./nowplaying');
-	} catch (e) {
-		nodecg.log.error('Failed to load "nowplaying" lib:', e.stack);
-		process.exit(1);
-	}
-
-	try {
-		require('./countdown');
-	} catch (e) {
-		nodecg.log.error('Failed to load "countdown" lib:', e.stack);
-		process.exit(1);
+		nodecg.log.warn('"firebase" is not defined in cfg/sgdq17-layouts.json! ' +
+			'The interview question system (Lightning Round) will be disabled.');
 	}
 };
