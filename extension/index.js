0d17df3d4ea411e630e5717ceb2c5c2edcc53778
Alex Van Camp <vancamp.alex@gmail.com>
2017-06-21 21:49:32 -0500

    tracker: refresh login token every 110 minutes, due to sessions expiring after 120 minutes

--- extension/index.js
+++ extension/index.js
@@ -13,6 +13,8 @@ module.exports = function (nodecg) {
 	// Store a reference to this nodecg API context in a place where other libs can easily access it.
 	// This must be done before any other files are `require`d.
 	nodecgApiContext.set(nodecg);
+	const loginLog = new nodecg.Logger(`${nodecg.bundleName}:tracker`);
+	let isFirstLogin = true;
 
 	if (nodecg.bundleConfig.useMockData) {
 		nodecg.log.warn('WARNING! useMockData is true, you will not receive real data from the tracker!');
@@ -30,32 +32,12 @@ module.exports = function (nodecg) {
 	require('./intermissions');
 	require('./caspar');
 
-	// Fetch the login page, and run the response body through cheerio
-	// so we can extract the CSRF token from the hidden input field.
-	// Then, POST with our username, password, and the csrfmiddlewaretoken.
-	request({
-		uri: LOGIN_URL,
-		transform(body) {
-			return cheerio.load(body);
-		}
-	}).then($ => request({
-		method: 'POST',
-		uri: LOGIN_URL,
-		form: {
-			username: nodecg.bundleConfig.tracker.username,
-			password: nodecg.bundleConfig.tracker.password,
-			csrfmiddlewaretoken: $('#login-form > input[name="csrfmiddlewaretoken"]').val()
-		},
-		headers: {
-			Referer: LOGIN_URL
-		},
-		resolveWithFullResponse: true,
-		simple: false
-	})).then(() => {
+	loginToTracker().then(() => {
 		require('./schedule');
-	}).catch(err => {
-		nodecg.log.error('Error authenticating with tracker!\n', err);
 	});
+
+	// Tracker logins expire every 2 hours. Re-login every 110 minutes.
+	setInterval(loginToTracker, 110 * 60 * 1000);
 
 	if (nodecg.bundleConfig.twitch) {
 		require('./twitch-ads');
@@ -87,5 +69,45 @@ module.exports = function (nodecg) {
 	} else {
 		nodecg.log.warn('"firebase" is not defined in cfg/sgdq17-layouts.json! ' +
 			'The interview question system (Lightning Round) will be disabled.');
+	}
+
+	// Fetch the login page, and run the response body through cheerio
+	// so we can extract the CSRF token from the hidden input field.
+	// Then, POST with our username, password, and the csrfmiddlewaretoken.
+	function loginToTracker() {
+		if (isFirstLogin) {
+			loginLog.info('Logging in as %s...', nodecg.bundleConfig.tracker.username);
+		} else {
+			loginLog.info('Refreshing tracker login session as %s...', nodecg.bundleConfig.tracker.username);
+		}
+
+		return request({
+			uri: LOGIN_URL,
+			transform(body) {
+				return cheerio.load(body);
+			}
+		}).then($ => request({
+			method: 'POST',
+			uri: LOGIN_URL,
+			form: {
+				username: nodecg.bundleConfig.tracker.username,
+				password: nodecg.bundleConfig.tracker.password,
+				csrfmiddlewaretoken: $('#login-form > input[name="csrfmiddlewaretoken"]').val()
+			},
+			headers: {
+				Referer: LOGIN_URL
+			},
+			resolveWithFullResponse: true,
+			simple: false
+		})).then(() => {
+			if (isFirstLogin) {
+				isFirstLogin = false;
+				loginLog.info('Logged in as %s.', nodecg.bundleConfig.tracker.username);
+			} else {
+				loginLog.info('Refreshed session as %s.', nodecg.bundleConfig.tracker.username);
+			}
+		}).catch(err => {
+			loginLog.error('Error authenticating!\n', err);
+		});
 	}
 };
