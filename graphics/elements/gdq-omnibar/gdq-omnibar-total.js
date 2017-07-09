91d5ca01dfbdb70def0afda715573029c3c42b00
Alex Van Camp <vancamp.alex@gmail.com>
2017-07-08 19:00:02 -0500

    omnibar: add bits total

--- graphics/elements/gdq-omnibar/gdq-omnibar-total.js
+++ graphics/elements/gdq-omnibar/gdq-omnibar-total.js
@@ -3,6 +3,7 @@
 
 	const TIME_PER_DOLLAR = 0.03;
 	const total = nodecg.Replicant('total');
+	const bitsTotal = nodecg.Replicant('bits:total');
 
 	class GdqOmnibarTotal extends Polymer.Element {
 		static get is() {
@@ -17,25 +18,32 @@
 			super.ready();
 			Polymer.RenderStatus.beforeNextRender(this, () => {
 				this.$.totalTextAmount.rawValue = 0;
-				total.on('change', this._handleTotalChanged.bind(this));
+				total.on('change', newVal => {
+					this._handleTotalChanged(this.$.totalTextAmount, newVal.raw);
+				});
+
+				this.$.bitsTotalAmount.rawValue = 0;
+				bitsTotal.on('change', newVal => {
+					this._handleTotalChanged(this.$.bitsTotalAmount, newVal);
+				});
 			});
 		}
 
-		_handleTotalChanged(newVal) {
+		_handleTotalChanged($element, newTotal) {
 			if (!this._totalInitialized) {
 				this._totalInitialized = true;
-				this.$.totalTextAmount.rawValue = newVal.raw;
-				this.$.totalTextAmount.textContent = this.formatRawValue(newVal.raw);
+				$element.rawValue = newTotal;
+				$element.textContent = this.formatRawValue(newTotal);
 				return;
 			}
 
-			const delta = newVal.raw - this.$.totalTextAmount.rawValue;
+			const delta = newTotal - $element.rawValue;
 			const duration = Math.min(delta * TIME_PER_DOLLAR, 3);
-			TweenLite.to(this.$.totalTextAmount, duration, {
-				rawValue: newVal.raw,
+			TweenLite.to($element, duration, {
+				rawValue: newTotal,
 				ease: Power2.easeOut,
 				onUpdate() {
-					this.$.totalTextAmount.textContent = this.formatRawValue(this.$.totalTextAmount.rawValue);
+					$element.textContent = this.formatRawValue($element.rawValue);
 				},
 				callbackScope: this
 			});
